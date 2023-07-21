9ea9a8a23ad807a3fba29344746df9c47ed1d100
koraykoksal <kksl.koray@gmail.com>
2023-07-21 10:21:17 +0300

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,35 @@
 import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 
-export const Login = ({setuser}) => {
+export const Login = () => {
 
-    const [email, setEmail] = useState("")
-    const [pass, setPass] = useState("")
-    let navigate = useNavigate()
 
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        console.log(email)
-        console.log(pass)
-        
-        setuser({email,pass})
-        //kulanıcı login bilgilerini girdikten sonra navigate ile home sayfasına yönelendirli
-        //logout işlemi için setuser içini boşaltarak logout işlmei yapılır
-        navigate("/")
-    }
+
+
   return (
     
     <div className='container m-auto text-center p-3'>
 
         <h1 className='text-center text-danger mb-4'>Login</h1>
 
-        <form onSubmit={handleSubmit}>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" i
+        <form className='w-50 m-auto text-center'>
+
+            <div class="form-group mb-4">
+                <label for="exampleInputEmail1">Username</label>
+                <input type="text" class="form-control" i
                 d="exampleInputEmail1" 
                 aria-describedby="emailHelp" 
-                placeholder="Enter email" 
-                onChange={(e)=>setEmail(e.target.value)}/>
+                placeholder="Username"
+                />
             </div>
+
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" 
                 class="form-control" 
                 id="exampleInputPassword1" 
                 placeholder="Password"
-                onChange={(e)=>setPass(e.target.value)} />
+                />
             </div>
             <div className='mt-4'>
             <button type="submit" class="btn btn-primary">Login</button>
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -10,13 +10,12 @@ import NotFound from "../pages/NotFound";
 import Footer from "../components/Footer";
 import { FullStack } from "../pages/FullStack";
 import { Aws } from "../pages/Aws";
-import { PrivateRouter } from "./PrivateRouter";
 import { Login } from "../pages/Login";
-import { useState } from "react";
+import { PrivateRouter } from "./PrivateRouter";
 
 export const AppRouter = () => {
 
-  const [user, setuser] = useState(false)
+  // const [user, setuser] = useState(false)
   
   return (
     <>
@@ -28,16 +27,19 @@ export const AppRouter = () => {
         <Route index element={<FullStack/>}/>
         <Route path="aws" element={<Aws/>}/>
       </Route>
+
+      <Route element={<PrivateRouter/>}>
       <Route path="/people" element={<People />} />
+      </Route>
+      
+
       <Route path="/people/:id" element={<PersonDetail />} />
 
-      <Route element={<PrivateRouter user={user}/>}>
       <Route path="/contact" element={<Contact />} />
-      </Route>
       
       <Route path="*" element={<NotFound />} />
 
-      <Route path="/login" element={<Login setuser={setuser}/>}/>
+      <Route path="/login" element={<Login/>}/>
     </Routes>
     
     <Footer />
