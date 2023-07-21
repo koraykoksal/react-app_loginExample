e60d01775c417f934e16f2ade8aaec9926768470
koraykoksal <kksl.koray@gmail.com>
2023-07-21 10:18:28 +0300

    appRoute and MockApi

--- src/routers/PrivateRouter.jsx
+++ src/routers/PrivateRouter.jsx
@@ -3,13 +3,14 @@ import Contact from '../pages/Contact';
 import { Login } from '../pages/Login';
 import { Navigate, Outlet } from 'react-router-dom';
 
-export const PrivateRouter = ({user}) => {
+export const PrivateRouter = () => {
 
     // const user=false;
 
     //user kullanıcısı sisteme login olmadıysa /login sayfasına
     //yönlendirme yapmamızı sağlar.
-  return user ? <Outlet/> : <Navigate to="/login"/>
+  // return user ? <Outlet/> : <Navigate to="/login"/>
 
+  
   
 }
