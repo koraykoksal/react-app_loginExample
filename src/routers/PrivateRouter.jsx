ba4e238c4f3aff5860545f11cf04b38bd820eb58
koraykoksal <kksl.koray@gmail.com>
2023-07-21 10:40:24 +0300

    Navigate tag

--- src/routers/PrivateRouter.jsx
+++ src/routers/PrivateRouter.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Contact from '../pages/Contact';
 import { Login } from '../pages/Login';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 
 export const PrivateRouter = () => {
 
-    // const user=false;
+  const user = false;
 
-    //user kullanıcısı sisteme login olmadıysa /login sayfasına
-    //yönlendirme yapmamızı sağlar.
-  // return user ? <Outlet/> : <Navigate to="/login"/>
-
-  
+  return user ? <Outlet/> : <Navigate to="/login"/>
   
 }
